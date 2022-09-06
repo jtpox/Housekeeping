0344ad33885f7e795e2372cd144bb83f1b5eb0d4
jtpox <me@jtpox.com>
2022-09-06 21:36:35 +0800

    Added cors on backend.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const argon2 = require('argon2');
 const { name, version } = require('../package.json');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
